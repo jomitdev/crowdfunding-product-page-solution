9526f5693a9ada2df1167092e42392d9dc71f9c9
jomit <metrotasiosg@gmail.com>
2022-08-10 18:48:47 +0300

    Added functionality to the continue button and the bar

--- app/js/script.js
+++ app/js/script.js
@@ -4,8 +4,8 @@ const labelEl = document.querySelectorAll(".reward-label");
 const card = document.querySelectorAll("form > div");
 const indexesOfButtonsNotClicked = [];
 
-const dollarEl = document.querySelector(".test");
-const pledgeEl = document.querySelector(".pledge-input");
+const dollarEl = document.querySelectorAll(".test");
+const pledgeEl = document.querySelectorAll(".pledge-input");
 
 const paymentEl = document.querySelectorAll(".payment");
 
@@ -15,8 +15,54 @@ const closePopup = document.querySelector(".close-popup");
 const openPopup = document.querySelector(".button");
 const selectReward = document.querySelectorAll(".open-popup-with-option");
 
+const submitPledges = document.querySelectorAll("[type='button']");
+const moneyBackedEl = document.querySelector("#money");
+const totalBackersEl = document.querySelector("#backers");
+const bambooStandsLeftEl = document.querySelectorAll(".bamboo");
+const blackStandsLeftEl = document.querySelectorAll(".black");
+const mahoganysLeftEl = document.querySelectorAll(".mahogany");
+
+const barEl = document.querySelector(".inner-bar");
+
 let clicked = [false, false, false, false];
 let bookmarked = false;
+let moneyBacked = 89914;
+let totalBackers = 5007;
+let bambooStandsLeft = 101;
+let blackStandsLeft = 64;
+let mahoganysLeft = 0;
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+barEl.style.width = `${(money / 100000) * 100}%`;
+
+function calcBar(money) {
+  barEl.style.width = `${(money / 100000) * 100}%`;
+}
+
+function checkInput(amountGiven, minAmount) {
+  const amount = amountGiven.value;
+  if (amount <= 0) {
+    alert("Pledge amount cannot be less than 0");
+  } else if (amount < minAmount) {
+    alert(
+      `The minimum pledge amount for this option is $${minAmount}. You entered $${amount}.`
+    );
+  } else if (amount >= minAmount) {
+    alert(
+      "Payment succesful. Thank you for the help! You will be added to the backers list"
+    );
+    moneyBacked += Number(amount);
+    calcBar(moneyBacked);
+    moneyBacked = numberWithCommas(moneyBacked);
+    totalBackers += 1;
+    totalBackers = numberWithCommas(totalBackers);
+    moneyBackedEl.innerText = `$${moneyBacked}`;
+    totalBackersEl.innerText = totalBackers;
+  }
+}
 
 function getDocHeight() {
   let D = document;
@@ -51,9 +97,42 @@ window.addEventListener(
   false
 );
 
-dollarEl.addEventListener("click", function () {
-  pledgeEl.focus();
+submitPledges[0].addEventListener("click", function () {
+  checkInput(pledgeEl[0], 0.1);
 });
+submitPledges[1].addEventListener("click", function () {
+  checkInput(pledgeEl[1], 25);
+  if (bambooStandsLeft > 0) {
+    bambooStandsLeft -= 1;
+    for (let i = 0; i < bambooStandsLeftEl.length; i++) {
+      bambooStandsLeftEl[i].innerText = bambooStandsLeft;
+    }
+  }
+});
+submitPledges[2].addEventListener("click", function () {
+  checkInput(pledgeEl[2], 75);
+  if (blackStandsLeft > 0) {
+    blackStandsLeft -= 1;
+    for (let i = 0; i < blackStandsLeftEl.length; i++) {
+      blackStandsLeftEl[i].innerText = blackStandsLeft;
+    }
+  }
+});
+submitPledges[3].addEventListener("click", function () {
+  checkInput(pledgeEl[3], 200);
+  if (mahoganysLeft > 0) {
+    mahoganysLeft -= 1;
+    for (let i = 0; i < mahoganysLeftEl.length; i++) {
+      mahoganysLeftEl[i].innerText = mahoganysLeft;
+    }
+  }
+});
+
+for (let i = 0; i < dollarEl.length; i++) {
+  dollarEl[i].addEventListener("click", function () {
+    pledgeEl[i].focus();
+  });
+}
 
 closePopup.addEventListener("click", function () {
   popupCard.classList.add("close");
