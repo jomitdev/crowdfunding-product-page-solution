be53de30e714c458c3e4c77fbf196415a0f1d5fd
jomit <metrotasiosg@gmail.com>
2022-08-11 15:49:35 +0300

    Added functionality to the number of products left and made gray the ones that are unavailable

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,6 @@
+// Bugs: when nothing left does not get gray,
+// Add payment reward
+
 const bookmarkEl = document.querySelector(".bookmark");
 const radioEl = document.querySelectorAll('[type="radio"]');
 const labelEl = document.querySelectorAll(".reward-label");
@@ -24,13 +27,27 @@ const mahoganysLeftEl = document.querySelectorAll(".mahogany");
 
 const barEl = document.querySelector(".inner-bar");
 
+let valid = false;
+
+const aboutCard = document.querySelectorAll(".about > .card");
+
 let clicked = [false, false, false, false];
 let bookmarked = false;
 let moneyBacked = 89914;
+let moneyBackedWithCommas = 0;
 let totalBackers = 5007;
+let totalBackersWithCommas = 0;
 let bambooStandsLeft = 101;
 let blackStandsLeft = 64;
-let mahoganysLeft = 0;
+let mahoganysLeft = 2;
+
+let selectRewardArray = [bambooStandsLeft, blackStandsLeft, mahoganysLeft];
+let selectRewardArrayForPopup = [
+  0,
+  bambooStandsLeft,
+  blackStandsLeft,
+  mahoganysLeft,
+];
 
 function numberWithCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -39,28 +56,47 @@ function numberWithCommas(x) {
 barEl.style.width = `${(money / 100000) * 100}%`;
 
 function calcBar(money) {
-  barEl.style.width = `${(money / 100000) * 100}%`;
+  let percentage = (money / 100000) * 100;
+  if (percentage <= 100) {
+    barEl.style.width = `${percentage}%`;
+  } else {
+    barEl.style.width = "100%";
+  }
+}
+
+for (let i = 0; i < selectRewardArray.length; i++) {
+  if (selectRewardArray[i] === 0) {
+    radioEl[i + 1].classList.remove("radio");
+    labelEl[i + 1].classList.remove("reward-label");
+  }
 }
 
 function checkInput(amountGiven, minAmount) {
   const amount = amountGiven.value;
   if (amount <= 0) {
-    alert("Pledge amount cannot be less than 0");
+    alert("The amount cannot be less than 0");
   } else if (amount < minAmount) {
     alert(
       `The minimum pledge amount for this option is $${minAmount}. You entered $${amount}.`
     );
   } else if (amount >= minAmount) {
+    valid = true;
     alert(
       "Payment succesful. Thank you for the help! You will be added to the backers list"
     );
+    moneyBacked = Number(moneyBacked);
     moneyBacked += Number(amount);
     calcBar(moneyBacked);
-    moneyBacked = numberWithCommas(moneyBacked);
+    moneyBackedWithCommas = moneyBacked;
+    console.log(moneyBackedWithCommas);
+    moneyBackedWithCommas = numberWithCommas(moneyBackedWithCommas).toString();
     totalBackers += 1;
-    totalBackers = numberWithCommas(totalBackers);
-    moneyBackedEl.innerText = `$${moneyBacked}`;
-    totalBackersEl.innerText = totalBackers;
+    totalBackersWithCommas = totalBackers;
+    totalBackersWithCommas = numberWithCommas(
+      totalBackersWithCommas
+    ).toString();
+    moneyBackedEl.innerText = `$${moneyBackedWithCommas}`;
+    totalBackersEl.innerText = totalBackersWithCommas;
   }
 }
 
@@ -102,29 +138,62 @@ submitPledges[0].addEventListener("click", function () {
 });
 submitPledges[1].addEventListener("click", function () {
   checkInput(pledgeEl[1], 25);
-  if (bambooStandsLeft > 0) {
+  if (bambooStandsLeft > 0 && valid) {
     bambooStandsLeft -= 1;
     for (let i = 0; i < bambooStandsLeftEl.length; i++) {
       bambooStandsLeftEl[i].innerText = bambooStandsLeft;
     }
   }
+
+  if (bambooStandsLeft === 0) {
+    submitPledges[1].classList.remove("button-smaller");
+    submitPledges[1].classList.add("button-gray");
+    submitPledges[1].innerHTML = "Out of stock";
+    card[1].classList.add("gray");
+    selectReward[0].classList.remove("button-smaller");
+    selectReward[0].classList.add("button-gray");
+    selectReward[0].innerHTML = "Out of stock";
+    aboutCard[0].classList.add("gray");
+  }
 });
 submitPledges[2].addEventListener("click", function () {
   checkInput(pledgeEl[2], 75);
-  if (blackStandsLeft > 0) {
+  if (blackStandsLeft > 0 && valid) {
     blackStandsLeft -= 1;
     for (let i = 0; i < blackStandsLeftEl.length; i++) {
       blackStandsLeftEl[i].innerText = blackStandsLeft;
     }
   }
+
+  if (blackStandsLeft === 0) {
+    submitPledges[2].classList.remove("button-smaller");
+    submitPledges[2].classList.add("button-gray");
+    submitPledges[2].innerHTML = "Out of stock";
+    card[2].classList.add("gray");
+    selectReward[1].classList.remove("button-smaller");
+    selectReward[1].classList.add("button-gray");
+    selectReward[1].innerHTML = "Out of stock";
+    aboutCard[1].classList.add("gray");
+  }
 });
 submitPledges[3].addEventListener("click", function () {
   checkInput(pledgeEl[3], 200);
-  if (mahoganysLeft > 0) {
+  if (mahoganysLeft > 0 && valid) {
     mahoganysLeft -= 1;
     for (let i = 0; i < mahoganysLeftEl.length; i++) {
       mahoganysLeftEl[i].innerText = mahoganysLeft;
     }
+  }
+
+  if (mahoganysLeft === 0) {
+    submitPledges[3].classList.remove("button-smaller");
+    submitPledges[3].classList.add("button-gray");
+    submitPledges[3].innerHTML = "Out of stock";
+    card[3].classList.add("gray");
+    selectReward[2].classList.remove("button-smaller");
+    selectReward[2].classList.add("button-gray");
+    selectReward[2].innerHTML = "Out of stock";
+    aboutCard[2].classList.add("gray");
   }
 });
 
@@ -147,50 +216,96 @@ openPopup.addEventListener("click", function () {
 });
 
 for (let i = 0; i < selectReward.length; i++) {
-  selectReward[i].addEventListener("click", function () {
-    for (let j = 0; j < clicked.length; j++) {
-      card[j].classList.remove("card-selected");
-      paymentEl[j].classList.add("payment");
-      radioEl[j].checked = false;
-    }
+  if (selectRewardArray[i] === 0) {
+    selectReward[i].classList.remove("button-smaller");
+    selectReward[i].classList.add("button-gray");
+    selectReward[i].innerHTML = "Out of stock";
+    aboutCard[i].classList.add("gray");
+    card[i + 1].classList.add("gray");
+  }
+}
 
-    const otherHeight = amountscrolled() + 1500;
-    popupCard.style.top = `${otherHeight}px`;
-    popupCard.classList.remove("close");
-    transparentBg.classList.remove("close");
-    card[i + 1].classList.add("card-selected");
-    paymentEl[i + 1].classList.remove("payment");
-    radioEl[i + 1].checked = true;
+for (let i = 0; i < selectReward.length; i++) {
+  selectReward[i].addEventListener("click", function () {
+    selectRewardArray = [bambooStandsLeft, blackStandsLeft, mahoganysLeft];
+    if (selectRewardArray[i] > 0) {
+      for (let j = 0; j < clicked.length; j++) {
+        card[j].classList.remove("card-selected");
+        paymentEl[j].classList.add("payment");
+        radioEl[j].checked = false;
+      }
+
+      const otherHeight = amountscrolled() + 1500;
+      popupCard.style.top = `${otherHeight}px`;
+      popupCard.classList.remove("close");
+      transparentBg.classList.remove("close");
+      card[i + 1].classList.add("card-selected");
+      paymentEl[i + 1].classList.remove("payment");
+      radioEl[i + 1].checked = true;
+    }
   });
 }
 
 for (let i = 0; i < radioEl.length; i++) {
   radioEl[i].addEventListener("mouseover", function () {
-    labelEl[i].classList.add("radio-button-hovered");
+    selectRewardArrayForPopup = [
+      0,
+      bambooStandsLeft,
+      blackStandsLeft,
+      mahoganysLeft,
+    ];
+    if (selectRewardArrayForPopup[i] != 0 || i === 0) {
+      labelEl[i].classList.add("radio-button-hovered");
+    } else {
+      radioEl[i].classList.remove("radio");
+      labelEl[i].classList.remove("reward-label");
+    }
   });
 
   radioEl[i].addEventListener("mouseout", function () {
-    labelEl[i].classList.remove("radio-button-hovered");
+    selectRewardArrayForPopup = [
+      0,
+      bambooStandsLeft,
+      blackStandsLeft,
+      mahoganysLeft,
+    ];
+    if (selectRewardArrayForPopup[i] != 0 || i === 0) {
+      labelEl[i].classList.remove("radio-button-hovered");
+    } else {
+      radioEl[i].classList.remove("radio");
+      labelEl[i].classList.remove("reward-label");
+    }
   });
 
   radioEl[i].addEventListener("click", function () {
-    clicked[i] === true;
-    // finds the index of the buttons that aren't clicked
-    for (let index = 0; index < clicked.length; index++) {
-      if (clicked[index] === false) {
-        indexesOfButtonsNotClicked.push(index);
+    selectRewardArrayForPopup = [
+      0,
+      bambooStandsLeft,
+      blackStandsLeft,
+      mahoganysLeft,
+    ];
+    if (selectRewardArrayForPopup[i] != 0 || i === 0) {
+      clicked[i] === true;
+      // finds the index of the buttons that aren't clicked
+      for (let index = 0; index < clicked.length; index++) {
+        if (clicked[index] === false) {
+          indexesOfButtonsNotClicked.push(index);
+        }
       }
+      // removes the cyan color from the labels that haven't been clicked
+      for (let j = 0; j < clicked.length; j++) {
+        labelEl[j].classList.remove("radio-button-clicked");
+        paymentEl[j].classList.add("payment");
+        card[j].classList.remove("card-selected");
+      }
+      // adds the cyan color to the label that has been clicked
+      labelEl[i].classList.add("radio-button-clicked");
+      paymentEl[i].classList.remove("payment");
+      card[i].classList.add("card-selected");
+    } else {
+      radioEl[i].classList.remove("radio");
+      labelEl[i].classList.remove("reward-label");
     }
-    // removes the cyan color from the labels that haven't been clicked
-    for (let j = 0; j < clicked.length; j++) {
-      labelEl[j].classList.remove("radio-button-clicked");
-      paymentEl[j].classList.add("payment");
-      card[j].classList.remove("card-selected");
-    }
-    // adds the cyan color to the label that has been clicked
-    labelEl[i].classList.add("radio-button-clicked");
-    paymentEl[i].classList.remove("payment");
-    card[i].classList.add("card-selected");
   });
 }
 
